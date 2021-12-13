43e10ef55557e0642bcdd17e928ee6197f848a60
US <umitsahin0@gmail.com>
2021-12-13 16:25:58 +0300

    Selenium search in Amazon

--- AmazonTest.java
+++ AmazonTest.java
@@ -19,25 +19,27 @@ public class AmazonTest {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.get("https://www.amazon.com/");
 
-        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
         searchBox.sendKeys("selenium");
         driver.findElement(By.id("nav-search-submit-button")).click();
 
-        String expectedResult = "1-48 of 304 results for \"Java\"";
+        String expectedResult = "results for \"selenium\"";
 
         WebElement actualResultText = driver.findElement(By.xpath("//*[@id=\"search\"]/span/div/span/h1/div/div[1]/div/div/span[1]"));
         String actualResult = actualResultText.getText();
         WebElement actualResultText2 = driver.findElement(By.xpath("//*[@id=\"search\"]/span/div/span/h1/div/div[1]/div/div/span[3]"));
-        System.out.println("actualResult = " + actualResult + " " + actualResultText2.getText());
+        String totalActualResult = actualResult + " " + actualResultText2.getText();
         Thread.sleep(3000);
 
-        driver.quit();
 
-        if (expectedResult.equals(actualResult)){
+        if (totalActualResult.contains(expectedResult)){
             System.out.println("PASS");
         }else {
+            System.out.println("Expected Result -> " + expectedResult);
+            System.out.println("Actual Result -> " + actualResult);
             System.out.println("FAIL");
         }
 
+        driver.quit();
     }
 }
